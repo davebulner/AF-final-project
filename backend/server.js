4ea491d97aad5a375c1f5cfdd04fcf168df83df6
Agasthi98 <sankalana98@gmail.com>
2021-05-27 23:32:56 +0530

    add port color

--- backend/server.js
+++ backend/server.js
@@ -14,4 +14,4 @@ app.get('/', (req,res)=>{
 
 const PORT = process.env.PORT || 8040
 
-app.listen(PORT, console.log(`server running on port ${PORT}`))
+app.listen(PORT, console.log(`server running on port ${PORT}`.brightYellow))
