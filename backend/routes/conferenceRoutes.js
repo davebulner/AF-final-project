a3600683891c54df285e53a491b7e0b0cf81ae80
Madush99 <madusankagajadeera99@gmail.com>
2021-05-29 00:23:03 +0530

    update Conference details

--- backend/routes/conferenceRoutes.js
+++ backend/routes/conferenceRoutes.js
@@ -2,9 +2,10 @@ import express from 'express'
 
 const router = express.Router()
 
-import { addConDetails, getAllConDetails } from '../controllers/conferenceController.js'
+import { addConDetails, getAllConDetails, updateConDetails } from '../controllers/conferenceController.js'
 
 router.post('/addConDetails', addConDetails)
 router.get('/', getAllConDetails)
+router.route('/:id').put(updateConDetails)
 
 export default router
