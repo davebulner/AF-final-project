dfef085cbbf9ffe2c21886f0252b201eb42a1641
Madush99 <madusankagajadeera99@gmail.com>
2021-05-28 10:16:41 +0530

    conference model

---
+++ backend/models/conferenceDetailsModel.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose'
+
+const conferenceDetailsSchema = mongoose.Schema({
+      user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+      },
+      conname: {
+            type: String,
+            required: true
+      },
+      description: {
+            type: String,
+            required: true
+      },
+      organizer: {
+            type: String,
+            required: true
+      },
+      phone: {
+            type: Number,
+            required: true
+      },
+      email: {
+            type: String,
+            required: true
+      },
+      startDate: {
+            type: Date
+      },
+      endDate: {
+            type: Date
+      },
+      venue: {
+            type: String,
+            required: true
+      }
+
+}, {
+      timestamps: true
+})
+
+const ConferenceDetails = mongoose.model('ConferenceDetails', conferenceDetailsSchema)
+
+export default ConferenceDetails
