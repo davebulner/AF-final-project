762f23a51b1cebbe713ff51e9029f70077aee01c
davebulner <davebulner3@gmail.com>
2021-05-28 17:13:27 +0545

    user login check

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,24 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 
+
+import userRoutes from './routes/userRoutes.js'
+
 dotenv.config()
+
 connectDB()
 
 const app = express()
 
+
+app.use(express.json())
+
 app.get('/', (req,res)=>{
     res.send('API running...')
 })
+
+app.use('/api/users', userRoutes)
+
 
 const PORT = process.env.PORT || 8040
 
