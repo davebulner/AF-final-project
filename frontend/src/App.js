c8d5bcf8c47d0f4dfa4a1c069ac9b6f7adfcdf46
Madush99 <madusankagajadeera99@gmail.com>
2021-06-05 00:32:12 +0530

    Nav bar completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import React from "react"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 import "./App.css"
+import Showcase from "./components/Showcase"
 
 function App() {
   return (
     <>
       <Navbar />
       <div className="main">
-
+        <Showcase />
 
       </div>
       <Footer />
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const Navbar = () => {
+
+      const [navlinkOpen, navlinkToggle] = useState(false);
+
+      const handleNavLinksToggle = () => {
+            navlinkToggle(!navlinkOpen)
+      }
+
+      const renderClasses = () => {
+            let classes = 'navlinks';
+
+            if (navlinkOpen) {
+                  classes += " active"
+            }
+            return classes;
+      }
+
       return <nav>
             <div className="logo">
                   <i className="fas fa-mountain"></i>
                   <h4>VCMS</h4>
             </div>
-            <ul className="navlinks">
+            <ul className={renderClasses()}>
                   <li className="link"><a href="#">Home</a></li>
                   <li className="link"><a href="#">About</a></li>
                   <li className="link"><a href="#">Services</a></li>
                   <li className="link"><a href="#">Contact us</a></li>
             </ul>
+            <div onClick={handleNavLinksToggle} className="hambuger-toggle">
+                  <i className="fas fa-bars fa-lg"></i>
+            </div>
       </nav>
 }
 
