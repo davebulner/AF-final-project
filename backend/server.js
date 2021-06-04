1ab45beb812103c9b89137917720d7f0cb925f71
Agasthi98 <sankalana98@gmail.com>
2021-06-04 13:50:28 +0530

    server.js edit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import connectDB from './config/db'
+import connectDB from './config/db.js'
 import colors from 'colors'
 import path from 'path'
 
