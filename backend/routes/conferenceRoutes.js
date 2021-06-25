e47402ab5392202ce4941fac1704340d78468825
Madush99 <madusankagajadeera99@gmail.com>
2021-06-25 09:52:21 +0530

    Conference Auth

--- backend/routes/conferenceRoutes.js
+++ backend/routes/conferenceRoutes.js
@@ -3,8 +3,9 @@ import express from 'express'
 const router = express.Router()
 
 import { addConDetails, getAllConDetails, updateConDetails } from '../controllers/conferenceController.js'
+import { protect, editor } from '../middleware/authMiddleware.js'
 
-router.post('/addConDetails', addConDetails)
+router.post('/addConDetails', protect, editor, addConDetails)
 router.get('/', getAllConDetails)
 router.route('/:id').put(updateConDetails)
 
