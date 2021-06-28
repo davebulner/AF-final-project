18b29c01d6d09ce391b7d9a83d218efd4ce34193
Madush99 <madusankagajadeera99@gmail.com>
2021-06-28 09:25:44 +0530

    editor screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,11 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { userLoginReducer } from '../src/reducers/userReducers.js'
-<<<<<<< HEAD
 import { conListReducer } from '../src/reducers/conferenceReducer.js'
-=======
->>>>>>> bfa507b79823cd985943216fa9418ada89e7d9a8
-
 const reducer = combineReducers({
     userLogin: userLoginReducer,
     listCon: conListReducer
