e21fa63fb9373e14a9d3f487f029d6212f8cfb7e
ushiniavindika <ushavindika@gmail.com>
2021-06-30 21:16:48 +0530

    reviwer action created

---
+++ frontend/src/action/reviwerAction.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import { WORKSHOP_LIST_REQUEST_REVIWER,
+    WORKSHOP_LIST_SUCCESS_REVIWER,
+    WORKSHOP_LIST_FAIL_REVIWER,
+    WORKSHOP_LIST_RESET_REVIWER
+} from '../constants/reviwerConstants.js'
+
+
+export const getAllWorkshops = () => async (dispatch, getState) => {
+    try {
+          dispatch({
+                type: WORKSHOP_LIST_REQUEST_REVIWER,
+          })
+          const {
+                userLogin: { userInfo },
+          } = getState()
+
+          const config = {
+                headers: {
+                      Authorization: `Bearer ${userInfo.token}`,
+                },
+          }
+
+          const { data } = await axios.get('http://localhost:8040/api/reviewdetails/', config)
+
+          dispatch({
+                type: WORKSHOP_LIST_SUCCESS_REVIWER,
+                payload: data
+          })
+    } catch (error) {
+          dispatch({
+                type: WORKSHOP_LIST_FAIL_REVIWER,
+                payload:
+                      error.response && error.response.data.message
+                            ? error.response.data.message
+                            : error.message,
+          })
+    }
+}
